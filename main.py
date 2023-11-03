5afa7c8ef3db8d5dd270f314b1c3832363f04e6f
AKSGAJ <gajula.akshaya@gmail.com>
2023-11-02 23:13:34 -0400

    Update main.py
    Method to filter through txt file and find recommended movies

--- main.py
+++ main.py
@@ -18,3 +18,14 @@ def ask_question():
             print("Wrong input. Try again.")
     
     pass
+
+def movie_recomendations(movie_file, user_movie_file):
+    with open(movie_file, 'r') as file1, open(user_movie_file, 'r') as file2:
+        friend_file = set(file1.readlines())
+        user_file = set(file2.readlines())
+    
+    non_duplicates = friend_file.symmetric_difference(user_file)
+    with open(output_file, 'w') as output:
+        output.writelines(non_duplicates)
+    return output_file
+    
