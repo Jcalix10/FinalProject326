72ca92441b989880e60c1e49da72f0a97f951a0b
AKSGAJ <gajula.akshaya@gmail.com>
2023-11-02 22:30:17 -0400

    Create main.py
    added basic code for asking the user questions

---
+++ main.py
@@ -0,0 +1,20 @@
+
+
+def ask_question():
+    print("What recomendations do you want?")
+    choice = input(" Choose an Option:" + 
+          "\n a. movie" +
+          "\n b. music" +
+          "\n c. books\n")
+    
+    while True:
+        if choice == 'a':
+            pass
+        elif choice == 'b':
+            pass
+        elif choice == 'b':
+            pass
+        else:
+            print("Wrong input. Try again.")
+    
+    pass
